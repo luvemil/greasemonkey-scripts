56470b73c5845e88c4d12285a494f0f086968637
luvemil <taran.marco@gmail.com>
2017-04-10 15:06:19 +0200

    Add basic filter

--- subscene.js
+++ subscene.js
@@ -9,13 +9,31 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
-$("#flagWrapper").after('<div><input type="text"><button id="mytoggle" type="button">Toggle</button></div>');
+$("#flagWrapper").after('<div><input id="myfilterquery" type="text"><button id="mytoggle" type="button">Toggle</button></div>');
 var rows = $("tr").filter(function(ix,el) {
   return $(this).find("span.l").text();
 });
 $("#mytoggle").click(function() {
   rows.filter(function(ix,el) {
     return $(this).find("span.l").text().trim() != "English";
+  })
+    .hide();
+});
+$("#myfilterquery").submit(function() {
+  var isValid = true;
+  try {
+    var inputRegex = new RegExp($(this).val());
+  } catch(e) {
+    isValid = false;
+  }
+
+  if(!isValid) {
+    window.console.log("Regex not valid");
+    return $(this);
+  }
+
+  rows.filter(function(ix,el) {
+    return ! $(this).children("td.a1 a span:nth-child(2)").text().trim().match(inputRegex);
   })
     .hide();
 });
