56d3c5564d1f0cb1e1035658e1a4d81ea394835d
luvemil <taran.marco@gmail.com>
2017-04-11 10:16:06 +0200

    Add HI filter

--- subscene.js
+++ subscene.js
@@ -20,6 +20,11 @@ $("#flagWrapper").after(
       <input type="checkbox" id="caseInsensitiveBox">\
       Case Insensitive\
     </label>\
+    <select id="earChoice">\
+      <option id="earDefault" value="">Any</option>\
+      <option value="hi">HI</option>\
+      <option value="nohi">No HI</option>\
+    </select>\
     <button id="filterButton" type="button">Filter</button>\
     <button id="clearButton" type="button">Clear</button>\
   </div>'
@@ -53,9 +58,18 @@ $("#filterButton").click(function() {
     isValid = false;
   }
 
-  // Get the status of the checkbox
+  // Get the language filter params
   do_filter = $("#langChoice").val() != "";
   var filter_lang = $("#langChoice").val();
+
+  // Get the hearing impairment filter params
+  do_hi = $("#earChoice").val() != "";
+  var filter_hi = $("#earChoice").val();
+  if(filter_hi == "hi") {
+    var hi_class = "a41";
+  } else if (filter_hi == "nohi") {
+    var hi_class = "a40";
+  }
 
   rows.hide()
     .filter(function(ix,el) {
@@ -70,12 +84,19 @@ $("#filterButton").click(function() {
       }
       return $(this).find("td.a1 a span:nth-child(2)").text().trim().match(inputRegex);
     })
+    .filter(function(ix,el) {
+      if(!do_hi) {
+        return true;
+      }
+      return $(this).find("td:nth-child(3)").attr('class') == hi_class;
+    })
     .show();
 });
 $("#clearButton").click(function() {
   rows.show();
   // restore the filters to the empy state
   $("#langChoice").val($("#langDefault").val());
+  $("#earChoice").val($("#earDefault").val());
   $("#caseInsensitiveBox").prop('checked',false);
   $("#myFilterQuery").val("");
 });
